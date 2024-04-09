cee4d5acafe29bb66f588c4711e767d5d94841d1
4rldur0 <ngim24863@gmail.com>
2024-04-10 04:41:05 +0900

    Finish Basic things

--- App.jsx
+++ App.jsx
@@ -1,61 +1,71 @@
-import React from "react";
-import { Image, Carousel } from 'antd';
+import React, { useState } from "react";
+import { Button, Carousel, Input, Select, Space } from 'antd';
 
 export default function App() {
+    const [data, setData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [type, setType] = useState('album');
+    const [limit, setLimit] = useState(1);
+    const options = [
+        { value: 'album', label: 'album' },
+        { value: 'track', label: 'track' },
+        { value: 'artist', label: 'artist' },
+    ];
     const carouselStyles = {
         "width": "640px",
-        "border": "solid 1px #000",
         "margin": "auto"
     };
-    const albums = [
-        {
-            "id": "6BzxX6zkDsYKFJ04ziU5xQ",
-            "name": "COWBOY CARTER",
-            "image_url": "https://i.scdn.co/image/ab67616d0000b2731572698fff8a1db257a53599",
-            "spotify_url": "https://open.spotify.com/album/6BzxX6zkDsYKFJ04ziU5xQ"
-        },
-        {
-            "id": "2UJwKSBUz6rtW4QLK74kQu",
-            "name": "BEYONCÉ [Platinum Edition]",
-            "image_url": "https://i.scdn.co/image/ab67616d0000b2730d1d6e9325275f104f8e33f3",
-            "spotify_url": "https://open.spotify.com/album/2UJwKSBUz6rtW4QLK74kQu"
-        },
-        {
-            "id": "6PeoltoiWQWCyWA0JBHVGN",
-            "name": "16 CARRIAGES",
-            "image_url": "https://i.scdn.co/image/ab67616d0000b273f5220893852002a2a3078bab",
-            "spotify_url": "https://open.spotify.com/album/6PeoltoiWQWCyWA0JBHVGN"
-        },
-        {
-            "id": "6oxVabMIqCMJRYN1GqR3Vf",
-            "name": "Dangerously In Love",
-            "image_url": "https://i.scdn.co/image/ab67616d0000b27345680a4a57c97894490a01c1",
-            "spotify_url": "https://open.spotify.com/album/6oxVabMIqCMJRYN1GqR3Vf"
-        },
-        {
-            "id": "2m1enA3YrMLVvR3q0MqLpL",
-            "name": "COWBOY CARTER",
-            "image_url": "https://i.scdn.co/image/ab67616d0000b2734903a9678d5664b9cd9a3fd8",
-            "spotify_url": "https://open.spotify.com/album/2m1enA3YrMLVvR3q0MqLpL"
-        }
-    ];
 
-    function albumToJSX(albumJSON) {
+    async function fetchData() {
+        const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
+        const url = `${baseURL}?q=${searchTerm}&type=${type}&limit=${limit}`;
+        const request = await fetch(url);
+        const responseData = await request.json();
+        setData(responseData);
+        console.log(responseData);
+    }
+
+    function jsonToJSX(JSON) {
         return (
-            <div key={albumJSON.id}>
-                <img src={albumJSON.image_url} />
-                <h3>{albumJSON.name}</h3>
+            <div key={JSON.id}>
+                <iframe
+                    key={JSON.id}
+                    src={`https://open.spotify.com/embed/${type}/${JSON.id}?utm_source=generator`}
+                    width="100%"
+                    border="0"
+                    height="352"
+                    frameBorder="0"
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy">
+                </iframe>
             </div>
         )
     }
 
     return (
-        <div style={carouselStyles}>
-            <Carousel dotPosition="top">
-                {
-                    albums.map(albumToJSX)
-                }
-            </Carousel>
-        </div>
+        <div>
+            <header>
+                <h1> Spotify Demo </h1>
+            </header>
+            <main>
+                <div>
+                    <Space direction="vertical" size="middle">
+                        <Space.Compact size="large">
+                            <Select style={{ width: '18%' }} defaultValue="album" onChange={value => setType(value)} options={options} />
+                            <Input style={{ width: '12%' }} type="number" defaultValue={1} onChange={e => setLimit(e.target.value)} placeholder="Limit" />
+                            <Input style={{ width: '70%' }} onChange={e => setSearchTerm(e.target.value)} placeholder="Search Term" />
+                            <Button onClick={fetchData}>Search</Button>
+                        </Space.Compact>
+                    </Space>
+                </div>
+                <div style={carouselStyles}>
+                    <Carousel dotPosition="top">
+                        {
+                            data.map(jsonToJSX)
+                        }
+                    </Carousel>
+                </div>
+            </main>
+        </div >
     );
 }
